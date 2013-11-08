7cc12681c1d5d4c3332cbef369e2a82a35a6141a
James Fulop <jameslynnfulop@gmail.com>
2013-11-08 13:24:11 -0500

    music shit

---
+++ Assets/Scripts/MixMasterRickScript.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var waaaa : AudioSource;
+var meatspace : AudioSource;
+var windWaaaTransform :AudioSource;
+var windWaaa : AudioSource;
+
+function Start () {
+
+}
+
+function Update () {
+
+
+}
+
+function OnTriggerEnter(other : Collider) {
+	if (other.transform.IsChildOf(transform) && other.name == "meatspace"){
+		meatspace.mute= true;
+		Debug.Log(meatspace.volume);
+	}
+	
+	if ( other.trasnform.isChild(transform) && other.name == "B"){
+		
+	}
+}
