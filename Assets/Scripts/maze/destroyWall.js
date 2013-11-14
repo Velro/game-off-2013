e87f279d6a4c882bb2ddadc282313fb2292e1ba4
James Fulop <jameslynnfulop@gmail.com>
2013-11-13 19:19:05 -0500

    rough maze2

---
+++ Assets/Scripts/maze/destroyWall.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var delay : float = 0;
+
+@System.NonSerializedAttribute
+var size : float = 0;
+
+function Start () {
+	
+}
+function OnTriggerEnter (other : Collider) {
+	Destroy(transform.gameObject, delay);
+	
+	/*
+	for (var child : Transform in transform) {
+		yield WaitForSeconds(0.25);
+    	Destroy(child.gameObject);
+	}
+	if (transform.childCount == 0){
+		yield WaitForSeconds(0.25);
+		Destroy(transform.gameObject);
+	}
+	*/
+}	
