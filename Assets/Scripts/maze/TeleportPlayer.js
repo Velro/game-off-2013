ad61dd30a09d3ee7c14de13f7cadffd8f4e67aa1
James Fulop <jameslynnfulop@gmail.com>
2013-11-19 05:37:03 -0500

    snapping, tower, converted Trevor's models to fbx

--- Assets/Scripts/maze/TeleportPlayer.js
+++ Assets/Scripts/maze/TeleportPlayer.js
@@ -5,8 +5,10 @@ var lookAt : Transform;
 function OnTriggerEnter (player : Collider){
 	if (player.gameObject.tag == "Player"){
 		var playerScript = player.GetComponent(CharacterMotor);
-		player.transform.position = destination.TransformPoint(Vector3.up);
+		player.transform.position = destination.TransformPoint(Vector3(0,0.18,0));
 		//playerScript.movement.velocity = Vector3.zero;
-		player.transform.LookAt(lookAt.TransformPoint(Vector3.zero));
+		if (lookAt != null){
+			player.transform.LookAt(lookAt.TransformPoint(Vector3.zero));
+		}
 	}
 }
---
+++ Assets/Scripts/tableSnap.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(other : Collider){
+	other.transform.parent = transform;
+	other.transform.localPosition = Vector3.zero;
+	other.transform.rotation.eulerAngles = Vector3(270, 0, 0);
+	var rigidbody : Rigidbody = other.GetComponent("Rigidbody");
+	rigidbody.useGravity = false;
+	rigidbody.isKinematic = true;
+	other.GetComponent(OnClickMove).enabled = false;
+}
