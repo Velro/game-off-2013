01d13e412735b76c2a9a52e41d9bbb3d23fa43f1
James Fulop <jameslynnfulop@gmail.com>
2013-11-24 19:29:19 -0500

    x fade, level fades, static objects, credits

---
+++ Assets/Scripts/FadeX.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var player : GameObject;
+var startingColor : Color;
+var endingColor : Color;
+var distance : float = 5;
+
+function Start () {
+	startingColor = renderer.material.color;
+	endingColor = Color(startingColor.r, startingColor.g, startingColor.b, 0.0);
+	renderer.material.color.a = 0;
+}
+
+function Update () {
+	var speed = Time.deltaTime;
+	if (Vector3.Distance(player.transform.position, transform.position) < distance){
+		renderer.material.color.a += speed;
+	} else {
+		if (renderer.material.color.a > 0) {
+			renderer.material.color.a -= speed;
+		}
+	}
+}
